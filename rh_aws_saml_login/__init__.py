78d2e73c79185d8ff258d10b80c7b3d31978ff1d
Christian Assing <cassing@redhat.com>
2025-02-21 14:34:07 +0100

    :sparkles: rh-aws-saml-login as library

--- rh_aws_saml_login/__init__.py
+++ rh_aws_saml_login/__init__.py
@@ -1,5 +1,12 @@
 """Expose the public API of the package."""
 
+from ._api import get_aws_credentials
+from ._exceptions import NoAwsAccountError, NoKerberosTicketError
 from ._models import AwsCredentials
 
-__all__ = ["AwsCredentials"]
+__all__ = [
+    "AwsCredentials",
+    "NoAwsAccountError",
+    "NoKerberosTicketError",
+    "get_aws_credentials",
+]
